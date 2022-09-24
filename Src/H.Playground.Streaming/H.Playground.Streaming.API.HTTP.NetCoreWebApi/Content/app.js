dc115415896fc01e632f9603bdf0f83dbc37e27d
Hintee <hintea_dan@yahoo.co.uk>
2022-09-24 22:47:04 +0300

    Implement Streaming JS Client

--- Src/H.Playground.Streaming/H.Playground.Streaming.API.HTTP.NetCoreWebApi/Content/app.js
+++ Src/H.Playground.Streaming/H.Playground.Streaming.API.HTTP.NetCoreWebApi/Content/app.js
@@ -1,16 +1,24 @@
 ﻿(async function (document, Streamer, JSON) {
 
-    const timeoutInSeconds = 15;
+    const defaultSeparator = '\r\n';
     const jsonStringSeparator = '\r\n,\r\n';
     const stringStreamUrl = `/stream/timestamp`;
     const jsonsStreamUrl = `/stream/DataEntries`;
+    const numbersStreamUrl = `/stream/Numbers`;
     const buttonStreamStrings = document.getElementById("ButtonStreamStrings");
     const buttonStreamJsons = document.getElementById("ButtonStreamJsons");
+    const buttonStreamNumbers = document.getElementById("ButtonStreamNumbers");
     const printCanvas = document.getElementById("PrintCanvas");
     const inputSecondsToRun = document.getElementById("InputSecondsToRun");
 
     async function stringChunkProcessor(chunkValue) {
-        printCanvas.append(`${chunkValue}\n`);
+        let values = chunkValue.split(defaultSeparator);
+        for (let i in values) {
+            let value = values[i];
+            if (!value) continue;
+            console.debug(value);
+            printCanvas.append(`${value}\n`);
+        }
         await Promise.resolve(true);
     }
 
@@ -37,6 +45,17 @@
         await Promise.resolve(true);
     }
 
+    async function numberChunkProcessor(chunkValue) {
+        let values = chunkValue.split(defaultSeparator);
+        for (let i in values) {
+            let value = values[i];
+            if (!value) continue;
+            console.debug(value);
+            printCanvas.append(`${value}\n`);
+        }
+        await Promise.resolve(true);
+    }
+
     buttonStreamStrings.addEventListener("click", async () => {
         printCanvas.innerText = '';
         await new Streamer(`${stringStreamUrl}?t=${inputSecondsToRun.value}`, stringChunkProcessor).Start();
@@ -45,6 +64,11 @@
     buttonStreamJsons.addEventListener("click", async () => {
         printCanvas.innerText = '';
         await new Streamer(`${jsonsStreamUrl}?t=${inputSecondsToRun.value}`, jsonChunkProcessor).Start();
+    });
+
+    buttonStreamNumbers.addEventListener("click", async () => {
+        printCanvas.innerText = '';
+        await new Streamer(`${numbersStreamUrl}?t=${inputSecondsToRun.value}`, numberChunkProcessor).Start();
     });
 
 })(this.document, this.window.Streamer, this.JSON);
